f04239d113642c783e7b61f788988271ae97e9e1
Felipe Espinoza <49495370+FelipeSebastianEspinoza@users.noreply.github.com>
2020-05-14 16:02:35 -0400

    incio laravel

---
+++ resources/views/usuarios.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+
+<table class="table table-hover">
+    <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Email</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($users as $user)
+        <tr>
+            <th scope="row">{{$user->id}}</th>
+            <td>{{$user->name}}</td>
+            <td>{{$user->email}}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
+@endsection
